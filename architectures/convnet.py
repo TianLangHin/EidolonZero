6583c3f45f67fee495098eceeb18d82ced8e52b6
TianLangHin <tianlanghin@gmail.com>
2025-04-03 19:31:33 +1100

    Fixed naming convention for PyTorch layers (2D -> 2d)

--- architectures/convnet.py
+++ architectures/convnet.py
@@ -15,23 +15,23 @@ class ConvNet(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.res_tower = torch.nn.Sequential(
-            torch.nn.Conv2D(18, 256, kernel_size=3, stride=1),
-            torch.nn.BatchNorm2D(256),
+            torch.nn.Conv2d(18, 256, kernel_size=3, stride=1),
+            torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
-            torch.nn.Conv2D(256, 256, kernel_size=3, stride=1),
-            torch.nn.BatchNorm2D(256),
+            torch.nn.Conv2d(256, 256, kernel_size=3, stride=1),
+            torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
         )
         self.value_head = torch.nn.Sequential(
-            torch.nn.Conv2D(256, 1, kernel_size=1, stride=1),
-            torch.nn.BatchNorm2D(1),
+            torch.nn.Conv2d(256, 1, kernel_size=1, stride=1),
+            torch.nn.BatchNorm2d(1),
             torch.nn.Flatten(),
             torch.nn.Linear(4 * 4 * 1, 1),
             torch.nn.Tanh(),
         )
         self.policy_head = torch.nn.Sequential(
-            torch.nn.Conv2D(256, 256, kernel_size=2, stride=1, padding=1),
-            torch.nn.BatchNorm2D(256),
+            torch.nn.Conv2d(256, 256, kernel_size=2, stride=1, padding=1),
+            torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
             torch.nn.Flatten(),
             torch.nn.Linear(5 * 5 * 256, 5000),
